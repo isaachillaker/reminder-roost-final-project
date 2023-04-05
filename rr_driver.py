a70507e040138195feacfa16669ddac40aac92d7
Isaac Hillaker <izacistan@protonmail.com>
2023-04-05 12:24:21 -0500

    Modified driver to relfect changes in Queue and Stack of User.

--- rr_driver.py
+++ rr_driver.py
@@ -1,11 +1,31 @@
 from rr_classes import User
 
-# Create a user with an invalid first name
-user1 = User("Bob", "Smith")
+# Create a user
+user = User("Bob", "Smith")
 
+# Add three '3' tasks for the user to complete
+user.add_task("Do homework", 2)
+user.add_task("Go grocery shopping", 3)
+user.add_task("Take kids to school", 1)
 
-print(user1.first_name, user1.last_name)
+# to_do_tasks (PQ)
+print("YOUR TASKS - (Queue)")
+user.to_do_tasks.print_queue()
 
+print("")
+
+# Complete highest priority task
+user.complete_task()
+
+# completed_tasks (Stack)
+print("COMPLETED TASKS - (Stack)")
+user.completed_tasks.print_stack()
+
+print("")
+
+# to_do_tasks (PQ)
+print("YOUR TASKS - (Queue)")
+user.to_do_tasks.print_queue()
 
 
 
