121d3fa40c029ff2ee6b1a55ad3ccf16cd329f9c
Isaac Hillaker <izacistan@protonmail.com>
2023-04-05 11:33:25 -0500

    Added unit tests for methods in Stack class. ALl passing.

--- rr_unittests.py
+++ rr_unittests.py
@@ -1,7 +1,7 @@
 from io import StringIO
 from unittest.mock import patch
 
-from rr_classes import User, PriorityQueue
+from rr_classes import User, PriorityQueue, Stack
 import unittest
 import sys
 
@@ -70,6 +70,57 @@ class TestPriorityQueue(unittest.TestCase):
         with patch('sys.stdout', new=StringIO()) as output:
             pq.print_queue()
             self.assertEqual(output.getvalue(), expected_output)
+
+
+class TestStack(unittest.TestCase):
+    def test_push_and_pop(self):
+        stack = Stack()
+        stack.push(1)
+        stack.push(2)
+        stack.push(3)
+        self.assertEqual(stack.pop(), 3)
+        self.assertEqual(stack.pop(), 2)
+        self.assertEqual(stack.pop(), 1)
+
+    def test_peek(self):
+        stack = Stack()
+        stack.push(1)
+        stack.push(2)
+        stack.push(3)
+        self.assertEqual(stack.peek(), 3)
+        stack.pop()
+        self.assertEqual(stack.peek(), 2)
+
+    def test_is_empty(self):
+        stack = Stack()
+        self.assertTrue(stack.is_empty())
+        stack.push(1)
+        self.assertFalse(stack.is_empty())
+        stack.pop()
+        self.assertTrue(stack.is_empty())
+
+    def test_print_stack(self):
+        stack = Stack()
+        stack.push(1)
+        stack.push(2)
+        stack.push(3)
+        with unittest.mock.patch('builtins.print') as mock_print:
+            stack.print_stack()
+            mock_print.assert_has_calls([
+                unittest.mock.call(3),
+                unittest.mock.call(2),
+                unittest.mock.call(1)
+            ])
+
+    def test_pop_empty_stack(self):
+        stack = Stack()
+        with self.assertRaises(IndexError):
+            stack.pop()
+
+    def test_peek_empty_stack(self):
+        stack = Stack()
+        with self.assertRaises(IndexError):
+            stack.peek()
 
 
 if __name__ == '__main__':
