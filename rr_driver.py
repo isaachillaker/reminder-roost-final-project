be0f5168044a35c1f68a72324dd5e0363dc1cba3
Isaac Hillaker <izacistan@protonmail.com>
2023-04-14 21:55:15 -0500

    Altered driver. Created a GUI object to run the program and test the GUI's functionality.

--- rr_driver.py
+++ rr_driver.py
@@ -1,59 +1,17 @@
-from rr_classes import User
+from rr_classes import User, GUI
+from tkinter import Tk
 
-# Create a user
-user = User("Bob", "Smith")
 
-# Add three '3' tasks for the user to complete
-user.add_task("Do homework", 2)
-user.add_task("Go grocery shopping", 3)
-user.add_task("Take kids to school", 1)
+def main():
+    # create root window
+    root = Tk()
 
-# to_do_tasks (PQ)
-print("YOUR TASKS - (Queue)")
-user.to_do_tasks.print_queue()
+    # create GUI object and pass in root window
+    gui = GUI(root)
 
-print("")
+    # start the event loop
+    root.mainloop()
 
-# Complete highest priority task
-user.complete_task("Do homework")
 
-# completed_tasks (Stack)
-print("COMPLETED TASKS - (Stack)")
-user.completed_tasks.print_stack()
-
-print("")
-
-# to_do_tasks (PQ)
-print("YOUR TASKS REMAINING - (Queue)")
-user.to_do_tasks.print_queue()
-
-print("")
-
-# Delete a task
-print("TASK DELETED")
-user.delete_task("Go grocery shopping")
-
-print("")
-
-# to_do_tasks (PQ)
-print("YOUR TASKS REMAINING - (Queue)")
-user.to_do_tasks.print_queue()
-
-print("")
-
-# to_do_tasks (PQ)
-print("PRINT DELETED TASKS - (Stack)")
-user.deleted_tasks.print_stack()
-
-print("")
-
-user.undo()
-
-print("PRINT DELETED TASKS - (Stack)")
-user.deleted_tasks.print_stack()
-
-print("")
-
-# to_do_tasks (PQ)
-print("YOUR TASKS REMAINING - (Queue)")
-user.to_do_tasks.print_queue()
+if __name__ == '__main__':
+    main()
