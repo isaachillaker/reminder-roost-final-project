8327d888aaf86c022cb91069061c899831a8d503
Isaac Hillaker <izacistan@protonmail.com>
2023-04-22 22:51:56 -0500

    Commented out GUI calls Needed to use the consoel for debuggin stuff

--- rr_driver.py
+++ rr_driver.py
@@ -2,6 +2,29 @@ from rr_classes import User, GUI
 from tkinter import Tk
 
 
+
+
+
+# user = User("Isaac", "Hillaker")
+# user.add_task("A", 1)
+# user.add_task("B", 2)
+# user.add_task("C", 3)
+# print(user.to_do_tasks._queue)
+# print(user.to_do_tasks.print_queue())
+#
+# print("")
+#
+# user.delete_task("C")
+# print(user.to_do_tasks._queue)
+# print(user.to_do_tasks.print_queue())
+#
+# print("")
+#
+# user.undo()
+#
+# print(user.to_do_tasks._queue)
+# print(user.to_do_tasks.print_queue())
+
 def main():
     # create root window
     root = Tk()
