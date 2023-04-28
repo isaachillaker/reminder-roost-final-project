19dc789b5b0bb28af52df9d9350d4ace3ca8fb0e
Isaac Hillaker <izacistan@protonmail.com>
2023-04-28 01:59:58 -0500

    All unit tests completed and passing. Added additional documentation. Program is finished.

--- rr_classes.py
+++ rr_classes.py
@@ -4,6 +4,21 @@ from tkinter import *
 
 
 class User:
+    """
+    This User class represents a user of the task manager program.
+
+    This class stores tasks unique to the user by utilizing the PriorityQueue and Stack classes and creating objects
+    of them for storing data.
+
+    The 'to_do_tasks' attribute is a PriorityQueue object, where user tasks are saved as tuples with names,
+    priority levels, and indexes.
+
+    The 'completed_tasks' attribute is a Stack object where tasks that are completed are moved to.
+
+    The 'deleted_tasks' attribute is a Stack object too, where deleted tasks are moved (tasks that were
+    deleted before they could be completed).
+
+    """
     def __init__(self, first_name=None, last_name=None):
         if first_name is None:
             first_name = self.get_valid_name("Enter your first name:")
@@ -15,7 +30,6 @@ class User:
         self.completed_tasks = Stack()
         self.deleted_tasks = Stack()
 
-    # MISSING UNIT TEST!
     def get_valid_name(self, name):
         """
         This method is called when a new User object is created and no values are assigned to
@@ -31,7 +45,6 @@ class User:
             except ValueError as e:
                 print(e)
 
-    # MISSING UNIT TEST!
     def validate_name(self, name):
         """
         This method is used to validate the 'first_name' and 'last_name' attributes
@@ -45,7 +58,7 @@ class User:
 
     def add_task(self, task=None, priority=None):
         """
-        THis method adds a task to the 'to_do_tasks' PQ object.
+        This method adds a task to the 'to_do_tasks' PQ object.
 
         1) An error msg is returned if one of the arguments is left blank.
 
@@ -105,108 +118,187 @@ class User:
             if item == task:
                 self.to_do_tasks._queue.remove((priority, index, item))
                 self.deleted_tasks.push((-priority, index, item))
-                return (priority, index, item)
+                return priority, index, item
 
     def undo(self):
+        """
+        This method undoes a deletion by removing the last item added to the 'deleted_tasks' Stack object.
+
+        1) If the length of 'deleted_tasks' is greater than 0...
+        2) Unpack the last tuple stored in the 'deleted_tasks' Stack and run the pop() method on it.
+        3) Add the task (name) and its priority level to the 'to_do_tasks' Stack object.
+        """
+        # 1
         if len(self.deleted_tasks) > 0:
+            # 2
             priority, index, task = self.deleted_tasks.pop()
+            # 3
             self.to_do_tasks.push(task, priority)
             return task
 
 
 class PriorityQueue:
+    """
+    This class implements a priority queue data structure using the heapq module, which relies on Python lists.
+
+    The queue is initialized as an empty list and an index is set to 0. The index is used to ensure that items
+    with the same priority are inserted in the order they were added
+    """
     def __init__(self):
         self._queue = []
         self._index = 0
 
     def push(self, item, priority):
+        """
+        Adding a task in this program requires a task name (item) and a priority level (priority) which must be 1, 2,
+        or 3.
+
+        This method takes an item and priority as arguments. The heappush method (built-in to heapq) inserts the item
+        into the queue as a tuple, where a String (item) an Integer (priority), and an index are stored.
+        """
         heapq.heappush(self._queue, (-priority, self._index, item))
         self._index += 1
 
     def pop(self):
+        """
+        Returns the top item from the queue
+        """
         return heapq.heappop(self._queue)[-1]
 
     def print_queue(self):
+        """
+        Iterates through the queue, returning all its items.
+        """
         for _, _, item in sorted(self._queue):
             print(item)
 
     def queue(self):
+        """
+        Returns a list of items in the queue by their priority.
+        """
         return [item for _, _, item in sorted(self._queue)]
 
     def __len__(self):
+        """
+        Returns the length of the priority queue.
+        """
         return len(self._queue)
 
 
 class Stack:
+    """
+    This class implements a simple Stack data structure, based on the 'Last In First Out' mantra. It relies heavily
+    on Python lists, which are ideal for implementing Stacks.
+
+    In this program this class is utilized as a 'completed_tasks' and 'deleted_tasks' objects in the User class.
+    """
     def __init__(self):
         self._items = []
 
     def push(self, item):
+        """
+        Adds a task (item) to the _items list.
+        """
         self._items.append(item)
 
     def pop(self):
+        """
+        Deletes and returns the 'top' item from the _items list.
+        """
         if len(self._items) == 0:
             raise IndexError('pop from empty stack')
         return self._items.pop()
 
     def peek(self):
+        """
+        Views the top task in the _items list, but does not remove or manipulate it.
+        """
         if len(self._items) == 0:
             raise IndexError('peek from empty stack')
         return self._items[-1]
 
     def is_empty(self):
+        """
+        Returns 'True' if the _items list is empty
+        """
         return len(self._items) == 0
 
-    # def print_stack(self):
-    #     for item in reversed(self._items):
-    #         return item
-
     def print_stack(self):
+        """
+        Returns the _items list for front end display.
+        """
         items_list = []
         for item in reversed(self._items):
             items_list.append(item)
         return items_list
 
     def __len__(self):
+        """
+        Provides the length of the stack.
+        """
         return len(self._items)
 
 
 class GUI:
     def __init__(self, root):
+        """
+        This class creates the GUI for the program. It utilizes the tkinter module to create buttons, entry inputs,
+        and listboxes to form the front end portion of the program.
+
+        This first section of the class deals with laying out all the GUI's components and linking them to other
+        relevant methods found within the GUI class.
+
+        The methods implemented in this class utilize the User object's built-in methods to update data in the back end
+        portion of the program.
+
+        The various components of the GUI have comments to explain what they are and what they are linked to.
+        """
+
+        # Initialize the GUI
         self.root = root
+
+        # Set the name and size of the window
         root.title("Task Manager")
         root.geometry('815x500')
 
-        self.user = User("Isaac", "Hillaker")  # create a User object named after me (the student!)
+        # Create a User object (named after me, the student!)
+        self.user = User("Isaac", "Hillaker")
 
-        #Heading Label
+        # Heading Label, displaying the User's name
         self.heading_label = Label(root, text=f"{self.user.first_name}'s tasks", font=("Helvetica", 18, "bold"))
         self.heading_label.place(x=10, y=5)
 
+        # Instructions on how to add a new task
         self.instructions_label = Label(root,
                                         text="Type in a task then click the 'Add Task' button to add your new task.",
                                         font=("Helvetica", 12))
         self.instructions_label.place(x=10, y=45)
 
-        # create task entry box and add task button
+        # *******************
+        # GET USER INPUT
+        # *******************
+
+        # Label for 'Task Name' input
         self.task_label_1 = Label(root,
                                   text="Task name:",
                                   font=("Helvetica", 12))
         self.task_label_1.place(x=10, y=70)
 
-        # TASK NAME ENTRY
+        # Task Name Entry (gather user input for Task NAME)
         self.task_1_entry = Entry(root, width=25)
         self.task_1_entry.place(x=10, y=95)
 
+        # Error Label placeholder, empty and invisible by default. Displays an error message for bad input.
+        self.invalid_input_label = Label(root, fg="red")
+        self.invalid_input_label.place(x=175, y=150)
+
+        # Label for 'Priority Level' input
         self.task_label_2 = Label(root,
                                   text="Task priority level (1, 2, or 3):",
                                   font=("Helvetica", 12))
         self.task_label_2.place(x=10, y=120)
 
-        self.invalid_input_label = Label(root, fg="red", bg="green")
-        self.invalid_input_label.place(x=175, y=150)
-
-        # PRIORITY LEVEL ENTRY
+        # Priority Level Entry (gather user input for task PRIORITY Level)
         self.task_2_entry = Entry(root, width=25)
         self.task_2_entry.place(x=10, y=150)
 
@@ -214,29 +306,31 @@ class GUI:
         # BUTTONS
         # *******************
 
-        # ADD TASK button
+        # ADD TASK button (calls add_task() method)
         self.add_button = Button(root, width=10, height=2, bg="green", fg="white", text="Add New",
                                  command=self.add_task)
         self.add_button.place(x=10, y=175)
 
-        # UNDO button
+        # UNDO button (calls undo_deletion() method)
         self.undo_button = Button(root, width=10, height=2, bg="yellow", fg="black", text="Undo Deletion",
                                   command=self.undo_deletion)
         self.undo_button.place(x=100, y=175)
 
-        # COMPLETE TASK button
+        # COMPLETE TASK button (calls complete_task() method)
         self.complete_button = Button(root, width=10, height=2, bg="blue", fg="white", text="Complete Task",
-                                    command=self.complete_task)
+                                      command=self.complete_task)
         self.complete_button.place(x=10, y=435)
 
-        # DELETE TASK button
+        # DELETE TASK button (calls delete_task() method)
         self.remove_button = Button(root, width=10, height=2, bg="red", fg="white", text="Delete Task",
                                     command=self.delete_task)
         self.remove_button.place(x=100, y=435)
 
-        #*******************
+        # ************************
         # LISTBOXES & LABELS
-        #*******************
+        # This is where we can see the tasks we
+        # have created, completed, and deleted.
+        # ************************
 
         # TO-DO TASKS: Label & Listbox
         self.heading_label = Label(root, text="To-Do Tasks", font=("Helvetica", 18, "bold"))
@@ -258,6 +352,10 @@ class GUI:
 
         self.deleted_tasks_listbox = Listbox(root, width=50)
         self.deleted_tasks_listbox.place(x=500, y=250)
+
+    #*****************
+    # GUI METHODS
+    #*****************
 
     def add_task(self):
         """
@@ -330,22 +428,33 @@ class GUI:
             self.tasks_listbox.insert(END, name)
 
     def complete_task(self):
-        # Get the selected item from the tasks listbox
-        selected_item = self.tasks_listbox.get(self.tasks_listbox.curselection())
+        """
+        This method takes a task from the 'To-Do Listbox and moves it to the 'Completed Tasks' listbox. It also
+        updates the User's 'completed_tasks' Stack object.
 
-        # Add task to User object's Stack 'completed_tasks'
-        self.user.complete_task(selected_item)
+        1) Get the selected item from the tasks listbox, if any
+        2) Add task to User object's Stack 'completed_tasks'
+        3) Get the current version of 'completed_list' Stack
+        4) Update 'Completed Tasks Listbox' with the current items in User's 'completed_tasks' Stack object
+        5) Update the To-Do Listbox with User's current 'to_do_tasks' PQ object
+        """
+        # 1
+        if self.tasks_listbox.curselection():
+            selected_item = self.tasks_listbox.get(self.tasks_listbox.curselection())
 
-        # Get the current version of 'completed_list' Stack
-        completed_tasks = self.user.completed_tasks.print_stack()
+            # 2
+            self.user.complete_task(selected_item)
 
-        # Update 'Completed Tasks Listbox' with the current items in User's 'completed_tasks' Stack object
-        self.completed_listbox.delete(0, END)
-        for task in completed_tasks:
-            self.completed_listbox.insert(END, task)
+            # 3
+            completed_tasks = self.user.completed_tasks.print_stack()
 
-        # Update the To-Do Listbox with User's current 'to_do_tasks' PQ object
-        self.update_listbox()
+            # 4
+            self.completed_listbox.delete(0, END)
+            for task in completed_tasks:
+                self.completed_listbox.insert(END, task)
+
+            # 5
+            self.update_listbox()
 
     def delete_task(self):
         """
@@ -374,6 +483,12 @@ class GUI:
         self.update_listbox()
 
     def undo_deletion(self):
+        """
+        This method removes the last deleted task from the 'Deleted Tasks Listbox' and moves it back into the
+        'To Do Tasks' listbox. It also updates the User's 'to_do_tasks' PriorityQueue object and 'deleted_tasks'
+        Stack object. It then calls the update_listbox() method to update the listboxes with the most up-to-date
+        information.
+        """
         # Get last deleted item from User object's 'deleted_tasks' Stack
         self.user.undo()
 
--- rr_unittests.py
+++ rr_unittests.py
@@ -1,157 +1,246 @@
-from io import StringIO
-from unittest.mock import patch
-
-from rr_classes import User, PriorityQueue, Stack
 import unittest
-import sys
+from unittest.mock import patch
+from tkinter import Tk, END
+from rr_classes import User, PriorityQueue, Stack, GUI
 
 
-class UserTestCase(unittest.TestCase):
-    def test_create_user(self):
-        user = User('John', 'Doe')
-        self.assertEqual(user.first_name, 'John')
-        self.assertEqual(user.last_name, 'Doe')
-        self.assertEqual(len(user.to_do_tasks), 0)
-        self.assertEqual(len(user.completed_tasks), 0)
+class TestUser(unittest.TestCase):
 
-    # def test_get_valid_name(self):
+    def test_init(self):
+        user = User('Alice', 'Applebaum')
+        self.assertEqual(user.first_name, 'Alice')
+        self.assertEqual(user.last_name, 'Applebaum')
+        self.assertIsInstance(user.to_do_tasks, PriorityQueue)
+        self.assertIsInstance(user.completed_tasks, Stack)
+        self.assertIsInstance(user.deleted_tasks, Stack)
 
-    # def test_validate_name(self):
+    def test_get_valid_name(self):
+        with patch('builtins.input', return_value='Alice Applebaum'):
+            user = User()
+            name = user.get_valid_name("Enter a name: ")
+            self.assertEqual(name, 'Alice Applebaum')
+
+    def test_validate_name(self):
+        user = User("Alice", "Applebaum")
+        self.assertRaises(ValueError, user.validate_name, "123")
+        self.assertRaises(ValueError, user.validate_name, "Alice123")
+        self.assertRaises(ValueError, user.validate_name, "Alice?")
+        self.assertEqual(user.validate_name("Alice"), "Alice")
+        self.assertEqual(user.validate_name("Alice-Applebaum"), "Alice-Applebaum")
+        self.assertEqual(user.validate_name("Alice O'Neil"), "Alice O'Neil")
 
     def test_add_task(self):
-        user = User('John', 'Doe')
-        user.add_task('Task 1', 1)
-        user.add_task('Task 2', 2)
-        self.assertEqual(len(user.to_do_tasks), 2)
+        user = User("Alice", "Applebaum")
+        self.assertEqual(user.add_task(), "Both task and priority must be specified!")
+        self.assertEqual(user.add_task("task1"), "Both task and priority must be specified!")
+        self.assertEqual(user.add_task(priority=1), "Both task and priority must be specified!")
+        self.assertIsNone(user.add_task("task1", 1))
 
     def test_complete_task(self):
-        user = User('John', 'Doe')
-        user.add_task('Task 1', 1)
-        user.add_task('Task 2', 2)
-        completed_task = user.complete_task()
-        self.assertEqual(completed_task, 'Task 2')
-        self.assertEqual(len(user.to_do_tasks), 1)
-        self.assertEqual(len(user.completed_tasks), 1)
-        completed_task = user.complete_task()
-        self.assertEqual(completed_task, 'Task 1')
-        self.assertEqual(len(user.to_do_tasks), 0)
-        self.assertEqual(len(user.completed_tasks), 2)
-
-    def test_complete_task_no_tasks(self):
-        user = User('John', 'Doe')
-        completed_task = user.complete_task()
-        self.assertIsNone(completed_task)
+        user = User("Alice", "Applebaum")
+        self.assertIsNone(user.complete_task())
+        self.assertIsNone(user.complete_task("task1"))
+        user.add_task("task1", 1)
+        user.add_task("task2", 2)
+        self.assertEqual(user.complete_task(), "task2")
+        self.assertEqual(user.completed_tasks.peek(), "task2")
+        self.assertEqual(user.complete_task("task1"), "task1")
+        self.assertEqual(user.completed_tasks.peek(), "task1")
 
     def test_delete_task(self):
-        user = User("Bob", "Smith")
-        user.add_task("Task 1", 2)
-        user.add_task("Task 2", 3)
-        user.add_task("Task 3", 1)
-        # Delete 'task 1' and ensure that the length of 'to_do_tasks' changed too
-        self.assertEqual(user.delete_task("Task 1"), "Task 1")
-        self.assertEqual(len(user.to_do_tasks), 2)
-        # Ensure that length of 'deleted_tasks' is 1, because there should be 1 task in it
-        self.assertEqual(len(user.deleted_tasks), 1)
+        user = User("Alice", "Applebaum")
+        self.assertIsNone(user.delete_task("task1"))
+        user.add_task("task1", 1)
+        self.assertEqual(user.delete_task("task1"), (-1, 0, "task1"))
+        self.assertEqual(user.deleted_tasks.peek(), (1, 0, "task1"))
 
     def test_undo(self):
-        user = User("Bob", 'Smith')
-        user.add_task("Task 1", 2)
-        user.add_task("Task 2", 1)
-        user.add_task("Task 3", 3)
-
-        # Delete 'Task 3' and 'Task 2'
-        user.delete_task("Task 3")
-        user.delete_task("Task 2")
-
-        # Ensure that both tasks are moved into 'deleted_tasks'
-        self.assertEqual(len(user.deleted_tasks), 2)
-
-        # Undo Deletion, removing 'Task 2'
-        self.assertEqual(user.undo(), "Task 2")
-
-        # Check that one task remains in 'deleted_tasks'
-        self.assertEqual(len(user.deleted_tasks), 1)
+        user = User("Alice", "Applebaum")
+        self.assertIsNone(user.undo())
+        user.add_task("task1", 1)
+        user.add_task("task2", 2)
+        user.delete_task("task1")
+        self.assertEqual(user.undo(), "task1")
 
 
 class TestPriorityQueue(unittest.TestCase):
-    def test_push(self):
-        q = PriorityQueue()
-        q.push('task 1', 1)
-        q.push('task 2', 2)
-        q.push('task 3', 3)
-        self.assertEqual(len(q), 3)
+    def test_push_and_pop(self):
+        queue = PriorityQueue()
+        queue.push("item1", 2)
+        queue.push("item2", 1)
+        queue.push("item3", 3)
+        self.assertEqual(queue.pop(), "item3")
+        self.assertEqual(queue.pop(), "item1")
+        self.assertEqual(queue.pop(), "item2")
+        self.assertEqual(len(queue), 0)
 
-    def test_pop(self):
-        q = PriorityQueue()
-        q.push('task 1', 1)
-        q.push('task 2', 2)
-        q.push('task 3', 3)
-        self.assertEqual(q.pop(), 'task 3')
-        self.assertEqual(q.pop(), 'task 2')
-        self.assertEqual(q.pop(), 'task 1')
-        self.assertEqual(len(q), 0)
+    def test_len(self):
+        queue = PriorityQueue()
+        self.assertEqual(len(queue), 0)
+        queue.push("item1", 1)
+        self.assertEqual(len(queue), 1)
+        queue.push("item2", 2)
+        self.assertEqual(len(queue), 2)
+        queue.pop()
+        self.assertEqual(len(queue), 1)
 
     def test_print_queue(self):
-        pq = PriorityQueue()
-        pq.push('task 1', 1)
-        pq.push('task 2', 2)
-        pq.push('task 3', 3)
-        expected_output = "task 3\ntask 2\ntask 1\n"
-        with patch('sys.stdout', new=StringIO()) as output:
-            pq.print_queue()
-            self.assertEqual(output.getvalue(), expected_output)
+        queue = PriorityQueue()
+        queue.push("item1", 1)
+        queue.push("item2", 2)
+        queue.push("item3", 3)
+        queue.push("item4", 2)
+        queue.push("item5", 1)
+        queue.push("item6", 3)
+        queue.push("item7", 3)
+
+        expected_output = "item3\nitem6\nitem7\nitem2\nitem4\nitem1\nitem5\n"
+        self.assertEqual('\n'.join(queue.queue()), expected_output.strip())
+
+    def test_queue(self):
+        queue = PriorityQueue()
+        queue.push("item1", 1)
+        queue.push("item2", 2)
+        queue.push("item3", 3)
+        queue.push("item4", 2)
+        queue.push("item5", 1)
+        queue.push("item6", 3)
+        queue.push("item7", 3)
+        self.assertEqual(queue.queue(), ["item3", "item6", "item7", "item2", "item4", "item1", "item5"])
 
 
 class TestStack(unittest.TestCase):
-    def test_push_and_pop(self):
-        stack = Stack()
-        stack.push(1)
-        stack.push(2)
-        stack.push(3)
-        self.assertEqual(stack.pop(), 3)
-        self.assertEqual(stack.pop(), 2)
-        self.assertEqual(stack.pop(), 1)
+    def test_push_pop(self):
+        s = Stack()
+        s.push(1)
+        s.push(2)
+        s.push(3)
+        self.assertEqual(s.pop(), 3)
+        self.assertEqual(s.pop(), 2)
+        self.assertEqual(s.pop(), 1)
+        self.assertTrue(s.is_empty())
+        self.assertRaises(IndexError, s.pop)
 
     def test_peek(self):
-        stack = Stack()
-        stack.push(1)
-        stack.push(2)
-        stack.push(3)
-        self.assertEqual(stack.peek(), 3)
-        stack.pop()
-        self.assertEqual(stack.peek(), 2)
+        s = Stack()
+        s.push(1)
+        s.push(2)
+        s.push(3)
+        self.assertEqual(s.peek(), 3)
+        s.pop()
+        self.assertEqual(s.peek(), 2)
+        s.pop()
+        self.assertEqual(s.peek(), 1)
+        s.pop()
+        self.assertRaises(IndexError, s.peek)
 
     def test_is_empty(self):
-        stack = Stack()
-        self.assertTrue(stack.is_empty())
-        stack.push(1)
-        self.assertFalse(stack.is_empty())
-        stack.pop()
-        self.assertTrue(stack.is_empty())
+        s = Stack()
+        self.assertTrue(s.is_empty())
+        s.push(1)
+        self.assertFalse(s.is_empty())
+        s.pop()
+        self.assertTrue(s.is_empty())
 
     def test_print_stack(self):
-        stack = Stack()
-        stack.push(1)
-        stack.push(2)
-        stack.push(3)
-        with unittest.mock.patch('builtins.print') as mock_print:
-            stack.print_stack()
-            mock_print.assert_has_calls([
-                unittest.mock.call(3),
-                unittest.mock.call(2),
-                unittest.mock.call(1)
-            ])
+        s = Stack()
+        s.push(1)
+        s.push(2)
+        s.push(3)
+        self.assertEqual(s.print_stack(), [3, 2, 1])
+        s.pop()
+        self.assertEqual(s.print_stack(), [2, 1])
+        s.pop()
+        self.assertEqual(s.print_stack(), [1])
+        s.pop()
+        self.assertEqual(s.print_stack(), [])
+        self.assertTrue(s.is_empty())
 
-    def test_pop_empty_stack(self):
-        stack = Stack()
-        with self.assertRaises(IndexError):
-            stack.pop()
+    def test_len(self):
+        s = Stack()
+        self.assertEqual(len(s), 0)
+        s.push(1)
+        self.assertEqual(len(s), 1)
+        s.push(2)
+        self.assertEqual(len(s), 2)
+        s.push(3)
+        self.assertEqual(len(s), 3)
+        s.pop()
+        self.assertEqual(len(s), 2)
+        s.pop()
+        self.assertEqual(len(s), 1)
+        s.pop()
+        self.assertEqual(len(s), 0)
+        self.assertTrue(s.is_empty())
+        self.assertRaises(IndexError, s.pop)
 
-    def test_peek_empty_stack(self):
-        stack = Stack()
-        with self.assertRaises(IndexError):
-            stack.peek()
 
+class TestGUI(unittest.TestCase):
 
-if __name__ == '__main__':
-    unittest.main()
+    def setUp(self):
+        self.root = Tk()
+        self.gui = GUI(self.root)
+        self.gui.task_1_entry.insert(0, "Walk the dog")
+        self.gui.task_2_entry.insert(0, "1")
+
+    def test_add_task_valid_input(self):
+        self.gui.add_task()
+        self.assertEqual(self.gui.tasks_listbox.get(END), "Walk the dog")
+        self.assertEqual(self.gui.task_1_entry.get(), "")
+        self.assertEqual(self.gui.task_2_entry.get(), "")
+
+    def test_add_task_invalid_priority(self):
+        self.gui.task_2_entry.delete(0, END)
+        self.gui.task_2_entry.insert(0, "4")
+        self.gui.add_task()
+        self.assertEqual(self.gui.invalid_input_label.cget("text"), "Number out of range. Must be a 1, 2, or 3!")
+        self.assertEqual(self.gui.task_1_entry.get(), "Walk the dog")
+        self.assertEqual(self.gui.task_2_entry.get(), "4")
+
+    def test_add_task_invalid_input(self):
+        self.gui.task_2_entry.delete(0, END)
+        self.gui.task_2_entry.insert(0, "a")
+        self.gui.add_task()
+        self.assertEqual(self.gui.invalid_input_label.cget("text"), "Must be a number: 1, 2, or 3!")
+        self.assertEqual(self.gui.task_1_entry.get(), "Walk the dog")
+        self.assertEqual(self.gui.task_2_entry.get(), "a")
+
+    def test_update_listbox(self):
+        self.gui.add_task()
+        self.gui.task_1_entry.insert(0, "Buy groceries")
+        self.gui.task_2_entry.insert(0, "2")
+        self.gui.add_task()
+        expected_list = ["Buy groceries", "Walk the dog"]
+        self.assertEqual(list(self.gui.tasks_listbox.get(0, END)), expected_list)
+
+    def test_complete_task(self):
+        self.gui.add_task()
+        self.gui.complete_task()
+        self.assertEqual(list(self.gui.completed_listbox.get(0, END)), [])
+        self.assertEqual(list(self.gui.tasks_listbox.get(0, END)), ['Walk the dog'])
+
+    def test_delete_task(self):
+        self.gui.add_task()
+        self.gui.task_1_entry.insert(0, "Buy groceries")
+        self.gui.task_2_entry.insert(0, "2")
+        self.gui.add_task()
+        self.gui.tasks_listbox.selection_clear(0, END)
+        self.gui.tasks_listbox.select_set(0)
+        self.gui.delete_task()
+        expected_tasks_list = ["Buy groceries"]
+        expected_deleted_list = ["Walk the dog"]
+        self.assertEqual(list(self.gui.tasks_listbox.get(0, END)), expected_deleted_list)
+        self.assertEqual(list(self.gui.deleted_tasks_listbox.get(0, END)), expected_tasks_list)
+
+    def test_undo_deletion(self):
+        self.gui.add_task()
+        self.gui.task_1_entry.insert(0, "Buy groceries")
+        self.gui.task_2_entry.insert(0, "2")
+        self.gui.add_task()
+        self.gui.tasks_listbox.selection_clear(0, END)
+        self.gui.tasks_listbox.select_set(0)
+        self.gui.delete_task()
+        self.gui.undo_deletion()
+        expected_tasks_list = ["Buy groceries", "Walk the dog"]
+        expected_deleted_list = []
+        self.assertEqual(list(self.gui.tasks_listbox.get(0, END)), expected_tasks_list)
